01ec8a0edf5a0ce8f878aa1ee98131f152bc769a
Hugo Rodrigues <hrlima96@gmail.com>
2018-09-04 17:52:23 -0300

    spring project

---
+++ spring/src/main/java/br/com/hugo/spring/todo/ToDoController.java
@@ -0,0 +1,10 @@
+package br.com.hugo.spring.todo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/todos")
+public class ToDoController {
+	
+}
